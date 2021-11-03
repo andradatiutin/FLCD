385a8b60f519369b262a7f3440d36b520c47b4fc
andradatiutin <andrada5@yahoo.com>
2021-11-03 15:29:37 +0200

    FA

--- Lab1Mare/src/Main.java
+++ Lab1Mare/src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -7,10 +9,14 @@ public class Main {
         Codification codification = new Codification();
         PIF pif = new PIF();
         SymTable symTable = new SymTable(26);
+        FA faConstant = new FA();
+        faConstant = reafFAfromfile("constant.in");
+        FA faIdentifier = new FA();
+        faIdentifier = reafFAfromfile("identifier.in");
         int lineNumber = 1;
         int ok = 1;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("FLCD/Lab1Mare/ex1.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("ex3.txt"));
             String text = reader.readLine();
             while (text != null) {
                 List<String> tokens = codification.tokenGenerator(text);
@@ -18,7 +24,7 @@ public class Main {
                     String s = tokens.get(i);
                     if (codification.isSeparator(s) || codification.isReservedWord(s) || codification.isOperator(s)) {
                         pif.addToken(codification.getCodificationTable().get(s), -1);
-                    } else if (codification.isIdentifier(s)) {
+                    } else if (faIdentifier.isAccepted(s)) {
                         pif.addToken(0, symTable.add(s));
                     } else if (codification.isConstant(s)) {
                         pif.addToken(1, symTable.add(s));
@@ -36,7 +42,7 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        if(ok==1){
+        if (ok == 1) {
             System.out.println("Lexically correct!");
             BufferedWriter writer = new BufferedWriter(new FileWriter("pif.out"));
             BufferedWriter writer1 = new BufferedWriter(new FileWriter("ST.out"));
@@ -50,5 +56,55 @@ public class Main {
         System.out.println(pif);
         System.out.println(symTable);
         System.out.println(codification);
+    }
+
+    public static FA reafFAfromfile(String file) {
+        String[] Q = {};
+        String[] E = {};
+        HashMap<List<String>, List<String>> S = new HashMap<>();
+        String q0 = "";
+        String[] F = {};
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String text = reader.readLine();
+            while (text != null) {
+                text = text.replace(" ", "");
+                String[] elements = text.split("=");
+                String terms = elements[1].replace("{", "").replace("}", "");
+                if (elements[0].equals("Q")) {
+                    Q = terms.split(",");
+                } else if (elements[0].equals("E")) {
+                    E = terms.split(",");
+                } else if (elements[0].equals("S")) {
+                    String[] t = terms.replace("(", "|").split(",\\|");
+                    for (int i = 0; i < t.length; i++) {
+                        String[] r = t[i].split("->");
+                        String[] left = r[0].replace("|", "").replace(")", "").split(",");
+                        List<String> key = new ArrayList<>();
+                        for (int j = 0; j < left.length; j++) {
+                            key.add(left[j]);
+                        }
+                        if (S.containsKey(key)) {
+                            S.get(key).add(r[1]);
+                        } else {
+                            List<String> value = new ArrayList<>();
+                            value.add(r[1]);
+                            S.put(key, value);
+                        }
+                    }
+                } else if (elements[0].equals("q0"))
+                    q0 = terms;
+                else if (elements[0].equals("F"))
+                    F = terms.split(",");
+                else System.out.println("Invalid token!");
+                text = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return new FA(Q, E, S, q0, F);
     }
 }
