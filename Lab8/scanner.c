97b01b743ab8580ba72abc10c481c437c4da494e
andradatiutin <andrada5@yahoo.com>
2022-01-02 21:58:35 +0200

    Lab8

---
+++ Lab8/scanner.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "scanner.h"
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+
+int main(int argc, char **argv)
+{
+    int ntoken, vtoken;
+
+    ntoken = yylex();
+    while(ntoken)
+    {
+        printf("%d\n", ntoken);
+        if(ntoken == -1)
+            return 0;
+        ntoken = yylex();
+    }
+    return 0;
+}
